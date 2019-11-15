3e3266356b153e556b844825ae388d319188ee3e
T00011786 <t00011786@vipshop.com>
2019-11-15 18:54:51 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -7,16 +13,24 @@ module.exports = {
     }
   },
   chainWebpack: config => {
-    config
-      .entry('app')
-      .clear()
-      .add('./example/main.js')
+    // config
+    //   .entry('app')
+    //   .clear()
+    //   .add('./example/main.js')
 
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].template = './example/index.html'
-        return args
-      })
+    // config
+    //   .plugin('html')
+    //   .tap(args => {
+    //     args[0].template = './example/index.html'
+    //     return args
+    //   })
+
+    config.module
+      .rule('js')
+      .test(/\.m?jsx?$/)
+      .include
+      .add(resolve('src'))
+      .add(resolve('examples'))
+      .end()
   }
 }
